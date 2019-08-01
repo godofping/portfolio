74da30c7d755b7775fb0efe2f30bc3c6ffbb66a6
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-08-02 00:57:12 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>REX</title>
+	<title>Rex Louis Portfolio</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://cdnjs.﻿cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TimelineMax.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TimelineMax.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:200,400,800&display=swap" rel="stylesheet">
+
+
+
+
 </head>
 <body>
 
@@ -13,24 +18,57 @@
 		<nav>
 			<h3 id="logo">Rex Louis</h3>
 			<img class="hamburger" src="img/hamburger.svg" alt="hamburger">
+			
 		</nav>
 
-		<section>
+		<section class="first-section">
 			<div class="hero">
 				<img src="img/background.jpg" alt="background">
-				<h1 class="headline">Rex Louis Roncesvalles</h1>
+				<h1 class="headline">Hello, I'm <span id="name">Rex Louis Roncesvalles</span>. <br> I'm a <span id="work">ghost programmer</span>.</h1>
 			</div>
 		</section>
 	</header>
 
 
-	<div class="slider"></div>
+	<div class="slider" id="particles-js"></div>
+
+
+	<section class="second-section">
+
+		<div class="sola">
+
+			
+
+			<div class="aboutimg">
+					<img src="img/me.jpg" alt="rex louis">
+			</div>
+	
+			<div class="aboutp">
+					<h2>About</h2>
+					<p class="about-p">Rex Louis is a jobless guy who is very eager to have a first legit job related to his profession. He has been a ghost programmer for 3 years, making projects and thesis of others.</p>
+
+			</div>
+
+		</div>
+		
+			
+	
+	
+	</section>
+
 
 
 
 	<!-- <script src="https://unpkg.com/@barba/core"></script> -->
 
 	<script type="text/javascript" src="js/app.js"></script>
+	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+	<script type="text/javascript">
+		/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+	</script>
 
 	
 </body>
