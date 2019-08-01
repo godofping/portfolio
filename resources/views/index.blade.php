eebd61cb051f09789c646ed12d8ead8d41c13445
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-08-01 18:45:01 +0800

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>REX</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://cdnjs.﻿cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TimelineMax.min.js"></script>
+</head>
+<body>
+
+
+	<header>
+		<nav>
+			<h3 id="logo">Rex Louis</h3>
+			<img class="hamburger" src="img/hamburger.svg" alt="hamburger">
+		</nav>
+
+		<section>
+			<div class="hero">
+				<img src="img/background.jpg" alt="background">
+				<h1 class="headline">Rex Louis Roncesvalles</h1>
+			</div>
+		</section>
+	</header>
+
+
+	<div class="slider"></div>
+
+
+
+	<!-- <script src="https://unpkg.com/@barba/core"></script> -->
+
+	<script type="text/javascript" src="js/app.js"></script>
+
+	
+</body>
+
+
+
+</html>
