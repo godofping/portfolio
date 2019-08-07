8d6e9b5de1e8e8aaae186b6049a109dd77bb6f24
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-08-07 22:36:59 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,12 @@ const logo = document.querySelector('#logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
 
+// const aboutimg = document.querySelector('.aboutimg');
+// const aboutp = document.querySelector('.about-p');
+// const abouth2 = document.querySelector('.abouth2');
+// const sola = document.querySelector('.second-section');
+
+
 
 var tl = new TimelineMax({});
 
@@ -11,4 +17,13 @@ tl.fromTo(hero, 1 , {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
 .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
 .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-.fromTo(hamburger, 0.5, {opacity:0, x: 30}, {opacity:1, x: 0}, "-=0.5" );
+.fromTo(hamburger, 0.5, {opacity:0, x: 30}, {opacity:1, x: 0}, "-=0.5" )
+
+// .fromTo(sola, 0.5, {opacity:0}, {opacity:1}, "-=1.2" )
+// .fromTo(aboutimg, 0.5, {opacity:0, x: 30}, {opacity:1, x: 0}, "-=0.5" )
+// .fromTo(aboutp, 0.5, {opacity:0, x: 30}, {opacity:1, x: 0}, "-=0.5" )
+// .fromTo(abouth2, 0.5, {opacity:0, x: 30}, {opacity:1, x: 0}, "-=0.5" )
+
+
+
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,18 @@
 <html>
 <head>
 	<title>Rex Louis Portfolio</title>
+	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://cdnjs.﻿cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TimelineMax.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:200,400,800&display=swap" rel="stylesheet">
-
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.4/SmoothScroll.min.js">
+	
 
 
 
 </head>
 <body>
-
 
 	<header>
 		<nav>
@@ -35,40 +36,43 @@
 
 	<section class="second-section">
 
-		<div class="sola">
+		<div class="about">
 
-			
+			<div class="img-about">
+				
+				<img src="img/me.jpg" alt="rex louis">
 
-			<div class="aboutimg">
-					<img src="img/me.jpg" alt="rex louis">
 			</div>
 	
-			<div class="aboutp">
-					<h2>About</h2>
-					<p class="about-p">Rex Louis is a jobless guy who is very eager to have a first legit job related to his profession. He has been a ghost programmer for 3 years, making projects and thesis of others.</p>
+			<div class="p-about">
+
+				<h2 class="h2-about">WHO IS REX LOUIS?</h2>
+				<p class="description">Rex Louis is a jobless guy who is very eager to have a first legit job related to his profession. He has been a ghost programmer for 3 years, making projects and thesis of others.</p>
 
 			</div>
 
 		</div>
 		
-			
-	
-	
+	</section>
+
+	<section class="third-section">
+		<h1>Works</h1>
+
 	</section>
 
 
 
 
 	<!-- <script src="https://unpkg.com/@barba/core"></script> -->
-
 	<script type="text/javascript" src="js/app.js"></script>
 	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 	<script type="text/javascript">
 		/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+		particlesJS.load('particles-js', 'assets/particles.json', function() {
+		console.log('callback - particles.js config loaded');
+		});
 	</script>
+			
 
 	
 </body>
